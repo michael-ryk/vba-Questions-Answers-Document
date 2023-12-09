badbef752ef977fad2f77925d72cc9f04922266a
MichaelR <spider747@gmail.com>
2023-12-09 20:49:02 +0200

    Set Grade and date based on user choice

--- QuestionAnswerExcel.vb
+++ QuestionAnswerExcel.vb
@@ -1,54 +1,63 @@
 Option Explicit
 
+'Global Constants
+Const FirstColumnAreaToColor    As String = "B"
+Const LastColumnAreaToColor     As String = "E"
+Const GradeColumn               As String = "D"
+Const DateLastReviewColumn      As String = "E"
+Const DataFirstRow              As Integer = 7
+
+
 Sub QAE_SetGradeGreat()
-    Call QAE_UpdateGrade("G")
+    Call QAE_UpdateGrade("Good")
 End Sub
+
 Sub QAE_SetGradeGreatOk()
-    Call QAE_UpdateGrade("O")
+    Call QAE_UpdateGrade("Ok")
 End Sub
+
 Sub QAE_SetGradeGreatBad()
-    Call QAE_UpdateGrade("B")
+    Call QAE_UpdateGrade("Bad")
 End Sub
 
 Sub QAE_UpdateGrade(Grade As String)
+        
+    Dim today               As Date
+    Dim selectedRow         As Integer
+    Dim selectedRowGrade    As String
+    Dim promptAnswer        As Integer
     
-    Const startingRow As Integer = 7
-    Const dateColumn As String = "K"
-    Const GradeColumn As String = "J"
-    Dim today As Date
-    Dim currentRow As Integer
-    Dim cellValue As String
-    
-    currentRow = ActiveCell.row
+    selectedRow = ActiveCell.row
     today = Date
-    cellValue = Cells(currentRow, GradeColumn)
+    selectedRowGrade = Cells(selectedRow, GradeColumn)
     
-    If (currentRow < startingRow) Then Exit Sub
-    If (Cells(currentRow, dateColumn) = Date) Then
+    'Prevent out of boundaries select
+    If (selectedRow < DataFirstRow) Then Exit Sub
+    
+    'Warning if already revised today
+    If (Cells(selectedRow, DateLastReviewColumn) = Date) Then
         promptAnswer = MsgBox("You already set a grade today. You want rewrite?", vbQuestion + vbYesNo + vbDefaultButton2, "Already set")
     End If
     
     If (promptAnswer = 7) Then Exit Sub
-    If (promptAnswer = 6) Then cellValue = Left(cellValue, Len(cellValue) - 1)
+    If (promptAnswer = 6) Then selectedRowGrade = Left(selectedRowGrade, Len(selectedRowGrade) - 1)
     
-    Cells(currentRow, GradeColumn) = cellValue + Grade
-    Cells(currentRow, dateColumn) = Date
+    'Shift history area
+    Range("F" & selectedRow & ":I" & selectedRow).Copy
+    Range("G" & selectedRow & ":J" & selectedRow).PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
+                :=False, Transpose:=False
+    Cells(selectedRow, GradeColumn) = Grade
+    Cells(selectedRow, DateLastReviewColumn) = Date
 
 End Sub
 
 
 Sub QAE_ApplyConditionalFormating()
-    
     '-----------------------------------
     'Important precondition in Excel:
     'You should define named ranges: goodTimeout, okTimeout, badTimeout
     'They should be anywere in excel and it is just 1 cell each with number of days you want to set for color change
     '-----------------------------------
-    
-    Const FirstColumnAreaToColor    As String = "B"
-    Const LastColumnAreaToColor     As String = "E"
-    Const GradeColumn               As String = "D"
-    Const DateLastReviewColumn      As String = "E"
     
     'Set Active color range
     Dim StrongColorRange    As Range
@@ -62,7 +71,7 @@ Sub QAE_ApplyConditionalFormating()
     
     'New question answered bad
     LightColorRange.FormatConditions.Add Type:=xlExpression, Formula1:= _
-            "=AND($" & GradeColumn & "1=3,$" & DateLastReviewColumn & "1+badTimeout>TODAY())"
+            "=AND($" & GradeColumn & "1=""Bad"",$" & DateLastReviewColumn & "1+badTimeout>TODAY())"
     With LightColorRange.FormatConditions(1)
             .Interior.Color = RGB(255, 240, 240)
             .Font.Color = RGB(128, 128, 128)
@@ -71,7 +80,7 @@ Sub QAE_ApplyConditionalFormating()
     
     'New question answered Ok
     LightColorRange.FormatConditions.Add Type:=xlExpression, Formula1:= _
-            "=AND($" & GradeColumn & "1=2,$" & DateLastReviewColumn & "1+okTimeout>TODAY())"
+            "=AND($" & GradeColumn & "1=""Ok"",$" & DateLastReviewColumn & "1+okTimeout>TODAY())"
     With LightColorRange.FormatConditions(2)
             .Interior.Color = RGB(255, 250, 220)
             .Font.Color = RGB(128, 128, 128)
@@ -80,7 +89,7 @@ Sub QAE_ApplyConditionalFormating()
     
     'New question answered Good
     LightColorRange.FormatConditions.Add Type:=xlExpression, Formula1:= _
-            "=AND($" & GradeColumn & "1=1,$" & DateLastReviewColumn & "1+goodTimeout>TODAY())"
+            "=AND($" & GradeColumn & "1=""Good"",$" & DateLastReviewColumn & "1+goodTimeout>TODAY())"
     With LightColorRange.FormatConditions(3)
             .Interior.Color = RGB(230, 250, 230)
             .Font.Color = RGB(128, 128, 128)
@@ -91,7 +100,7 @@ Sub QAE_ApplyConditionalFormating()
     
     'Old question answered bad
     StrongColorRange.FormatConditions.Add Type:=xlExpression, Formula1:= _
-            "=AND($" & GradeColumn & "1=3,$" & DateLastReviewColumn & "1+badTimeout<=TODAY())"
+            "=AND($" & GradeColumn & "1=""Bad"",$" & DateLastReviewColumn & "1+badTimeout<=TODAY())"
     With StrongColorRange.FormatConditions(4)
             .Interior.Color = RGB(255, 199, 206)
             .Font.Color = RGB(0, 97, 0)
@@ -100,7 +109,7 @@ Sub QAE_ApplyConditionalFormating()
 
     'Old question answered Ok
     StrongColorRange.FormatConditions.Add Type:=xlExpression, Formula1:= _
-            "=AND($" & GradeColumn & "1=2,$" & DateLastReviewColumn & "1+okTimeout<=TODAY())"
+            "=AND($" & GradeColumn & "1=""Ok"",$" & DateLastReviewColumn & "1+okTimeout<=TODAY())"
     With StrongColorRange.FormatConditions(5)
             .Interior.Color = RGB(255, 235, 156)
             .Font.Color = RGB(156, 101, 0)
@@ -109,7 +118,7 @@ Sub QAE_ApplyConditionalFormating()
 
     'Old question answered Good
     StrongColorRange.FormatConditions.Add Type:=xlExpression, Formula1:= _
-            "=AND($" & GradeColumn & "1=1,$" & DateLastReviewColumn & "1+goodTimeout<=TODAY())"
+            "=AND($" & GradeColumn & "1=""Good"",$" & DateLastReviewColumn & "1+goodTimeout<=TODAY())"
     With StrongColorRange.FormatConditions(6)
             .Interior.Color = RGB(198, 239, 206)
             .Font.Color = RGB(0, 97, 0)
@@ -118,4 +127,3 @@ Sub QAE_ApplyConditionalFormating()
 
 
 End Sub
-
